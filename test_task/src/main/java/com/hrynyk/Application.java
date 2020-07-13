0f15a49cf81ee65220e791f1960ce1f2251e471a
GHzzz <vavanpro3@gmail.com>
2020-07-13 20:37:42 +0300

    test_task for nerdysoft

---
+++ test_task/src/main/java/com/hrynyk/Application.java
@@ -0,0 +1,11 @@
+package com.hrynyk;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
